b64e828a37e0146a9f537bcceea148e9464b1cde
Eduardo B. Costa <ebritto25@gmail.com>
2021-06-19 17:41:22 -0300

    commiting from old laptop

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /home/eduardo/ProjQt/emul_blocks/index.js: Unexpected token (3:9)\\n\\n\\u001b[0m \\u001b[90m 1 | \\u001b[39m\\u001b[90m//const Brick = require('./js/plugins/brick.js');\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 2 | \\u001b[39m\\u001b[90m//const Motor = require('./js/plugins/motor.js');\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 3 | \\u001b[39m\\u001b[36mimport\\u001b[39m \\u001b[33m*\\u001b[39m from \\u001b[32m'./js/plugins/brick.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   | \\u001b[39m         \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 | \\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 | \\u001b[39mlet my_motor \\u001b[33m=\\u001b[39m \\u001b[36mnew\\u001b[39m \\u001b[33mMotor\\u001b[39m()\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 6 | \\u001b[39m\\u001b[0m\\n    at Parser._raise (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:766:17)\\n    at Parser.raiseWithData (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:759:17)\\n    at Parser.raise (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:753:17)\\n    at Parser.unexpected (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:8966:16)\\n    at Parser.expectContextual (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:8932:41)\\n    at Parser.maybeParseStarImportSpecifier (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:12885:12)\\n    at Parser.parseImport (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:12802:41)\\n    at Parser.parseStatementContent (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:11531:27)\\n    at Parser.parseStatement (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:11431:17)\\n    at Parser.parseBlockOrModuleBlockBody (/home/eduardo/ProjQt/emul_blocks/node_modules/@babel/parser/lib/index.js:12013:25)\");\n\n//# sourceURL=webpack://emul_blocks/./index.js?");
+/******/ })()
+;
